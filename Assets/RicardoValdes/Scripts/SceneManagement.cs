2932fd69360d728244f5c2d7ed7b2d7a1601528e
Ricardo Valdes <ricardovaldes@everfall-games.com>
2021-12-08 21:37:52 -0500

    Fourth Commit - More progress on 5th, 6th and 7th objective.

--- Assets/RicardoValdes/Scripts/SceneManagement.cs
+++ Assets/RicardoValdes/Scripts/SceneManagement.cs
@@ -8,11 +8,13 @@ public class SceneManagement : MonoBehaviour //Handles Scene UI both in-game and
 {
     [Header("Inputs")]
     [SerializeField] GameObject settingsMenuParent;
+    [SerializeField] Animator sceneTransition;
     [SerializeField] TextMeshProUGUI featureMessage;
     [SerializeField] string[] featureMessages;
 
     [Header("Variables")]
     [SerializeField] int currentScene;
+    [SerializeField] bool isInSettings;
 
     void Start()
     {
@@ -20,6 +22,12 @@ public class SceneManagement : MonoBehaviour //Handles Scene UI both in-game and
         {
             featureMessage = GameObject.FindGameObjectWithTag("FeatureMessage").GetComponent<TextMeshProUGUI>();
         }
+
+        if (!sceneTransition)
+        {
+            sceneTransition = GameObject.FindGameObjectWithTag("SceneTransition").GetComponent<Animator>();
+        }
+
         featureMessage.text = featureMessages[Random.Range(0, featureMessages.Length)]; //Ricardo Dec. 8: Randomly selects a message as the feature message.
     }
 
@@ -40,7 +48,12 @@ public class SceneManagement : MonoBehaviour //Handles Scene UI both in-game and
         Application.Quit();
     }
 
-    public void ShowSettingsMenu(bool enableOrDisable) //Ricardo, Dec. 8: True = Enabled, False = Disabled
+    public void MusicVolume(float volume)
+    {
+
+    }
+
+    public void ShowSettingsMenu(bool enableOrDisable) //Ricardo, Dec. 8: True = Enabled, False = Disabled.
     {
         switch (enableOrDisable) //Ricardo, Dec. 8: Using a switch here only because its more aesthetic lol.
         {
@@ -48,12 +61,14 @@ public class SceneManagement : MonoBehaviour //Handles Scene UI both in-game and
                 //enable settings UI
                 //pause
                 //play audio
+                isInSettings = true;
                 return;
 
             case false:
                 //disable settings UI
                 //resume
                 //play audio
+                isInSettings = false;
                 return;
         }
     }
